bc86861fa7b2b7086b8fec1adaf57c06cedbb81d
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-11-20 15:28:00 +0100

    Create random move for computer and loop for player choose

--- PaperRockScissors/src/PaperRockScissors.java
+++ PaperRockScissors/src/PaperRockScissors.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class PaperRockScissors{
     public static void main(String[] args) {
-        
+        String[] rps = {"r","p","s"};
+        String computerMove = rps[new Random().nextInt(rps.length)]; //generowanie losowego ruchu przeciwnika
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Please enter your move (r, p, or s)");
+            String playerMove = scanner.nextLine();
+            if (playerMove.equals("r")||playerMove.equals("p")||playerMove.equals("s")){
+                break;
+            }
+            System.out.println(playerMove+ " is not a valid move");
+        }
     }
 }
+
