13cb13eb49f61b80375903299f4889da0ffd4232
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-11-20 15:45:08 +0100

    logic of the play

--- PaperRockScissors/src/PaperRockScissors.java
+++ PaperRockScissors/src/PaperRockScissors.java
@@ -5,15 +5,45 @@ public class PaperRockScissors{
     public static void main(String[] args) {
         String[] rps = {"r","p","s"};
         String computerMove = rps[new Random().nextInt(rps.length)]; //generowanie losowego ruchu przeciwnika
+        String playerMove;
         Scanner scanner = new Scanner(System.in);
         while(true){
             System.out.println("Please enter your move (r, p, or s)");
-            String playerMove = scanner.nextLine();
+            playerMove = scanner.nextLine();
             if (playerMove.equals("r")||playerMove.equals("p")||playerMove.equals("s")){
                 break;
             }
             System.out.println(playerMove+ " is not a valid move");
         }
+        System.out.println("Computer played: "+computerMove);
+        if(playerMove.equals(computerMove)){
+            System.out.println("The game was a tie!");
+        }
+        else if(playerMove.equals("r")){
+            if(computerMove.equals("p")){
+                System.out.println("You lose!");
+            }
+            else if (computerMove.equals("s")){
+                System.out.println("You Win!");
+            }
+        }
+        else if(playerMove.equals("p")){
+            if(computerMove.equals("r")){
+                System.out.println("You Win!");
+            }
+            else if (computerMove.equals("s")){
+                System.out.println("You lose!");
+            }
+        }
+        else if(playerMove.equals("s")){
+            if(computerMove.equals("p")){
+                System.out.println("You Win!");
+            }
+            else if (computerMove.equals("r")){
+                System.out.println("You lose!");
+            }
+        }
+        scanner.close();
     }
 }
 
