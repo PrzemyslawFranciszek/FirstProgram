203e466e1539b9280922c6a2fbf862442228ea97
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2022-11-20 15:54:12 +0100

    Create loop for multiple games

--- PaperRockScissors/src/PaperRockScissors.java
+++ PaperRockScissors/src/PaperRockScissors.java
@@ -3,47 +3,58 @@ import java.util.Scanner;
 
 public class PaperRockScissors{
     public static void main(String[] args) {
-        String[] rps = {"r","p","s"};
-        String computerMove = rps[new Random().nextInt(rps.length)]; //generowanie losowego ruchu przeciwnika
-        String playerMove;
         Scanner scanner = new Scanner(System.in);
         while(true){
-            System.out.println("Please enter your move (r, p, or s)");
-            playerMove = scanner.nextLine();
-            if (playerMove.equals("r")||playerMove.equals("p")||playerMove.equals("s")){
+            String[] rps = {"r","p","s"};
+            String computerMove = rps[new Random().nextInt(rps.length)]; //generowanie losowego ruchu przeciwnika
+            String playerMove;
+            
+            while(true){
+                System.out.println("Please enter your move (r, p, or s)");
+                playerMove = scanner.nextLine();
+                if (playerMove.equals("r")||playerMove.equals("p")||playerMove.equals("s")){
+                    break;
+                }
+                System.out.println(playerMove+ " is not a valid move");
+            }
+            System.out.println("Computer played: "+computerMove);
+            if(playerMove.equals(computerMove)){
+                System.out.println("The game was a tie!");
+            }
+            else if(playerMove.equals("r")){
+                if(computerMove.equals("p")){
+                    System.out.println("You lose!");
+                }
+                else if (computerMove.equals("s")){
+                    System.out.println("You Win!");
+                }
+            }
+            else if(playerMove.equals("p")){
+                if(computerMove.equals("r")){
+                    System.out.println("You Win!");
+                }
+                else if (computerMove.equals("s")){
+                    System.out.println("You lose!");
+                }
+            }
+            else if(playerMove.equals("s")){
+                if(computerMove.equals("p")){
+                    System.out.println("You Win!");
+                }
+                else if (computerMove.equals("r")){
+                    System.out.println("You lose!");
+                }
+            }
+            System.out.println("Play again? (y/n)");
+            String playAgain = scanner.nextLine();
+            if(!playAgain.equals("y")){
+                scanner.close();
                 break;
             }
-            System.out.println(playerMove+ " is not a valid move");
+
+            
         }
-        System.out.println("Computer played: "+computerMove);
-        if(playerMove.equals(computerMove)){
-            System.out.println("The game was a tie!");
-        }
-        else if(playerMove.equals("r")){
-            if(computerMove.equals("p")){
-                System.out.println("You lose!");
-            }
-            else if (computerMove.equals("s")){
-                System.out.println("You Win!");
-            }
-        }
-        else if(playerMove.equals("p")){
-            if(computerMove.equals("r")){
-                System.out.println("You Win!");
-            }
-            else if (computerMove.equals("s")){
-                System.out.println("You lose!");
-            }
-        }
-        else if(playerMove.equals("s")){
-            if(computerMove.equals("p")){
-                System.out.println("You Win!");
-            }
-            else if (computerMove.equals("r")){
-                System.out.println("You lose!");
-            }
-        }
-        scanner.close();
+        
     }
 }
 
